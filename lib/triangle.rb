1d9fbc7df7366444df90f67422ce631e19751ff4
Kellen <kellenbmay@gmail.com>
2020-12-08 18:46:05 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,78 +1,41 @@
-# require 'pry'
-
-# class Triangle
-#   # write code here
-
-#   attr_accessor :side_one, :side_two, :side_three
-
-#   def initialize(side_one, side_two, side_three)
-#     @side_one = side_one
-#     @side_two = side_two
-#     @side_three = side_three
-#   end
-
-# def valid?
-#   if side_one + side_two > side_three || side_two + side_three > side_one || side_one + side_three > side_two && side_one > 0 && side_two > 0 && side_three > 0  
-#     true
-#   else
-#     false
-#   end
-#   # binding.pry
-# end
-
-# def kind
-  
-#   if !valid?
-#       raise TriangleError
-#       puts error.message
-#    elsif side_one == side_two && side_two == side_three && side_one == side_three 
-#       :equilateral
-#   elsif side_one == side_two || side_one == side_three || side_two == side_three
-#       :isosceles
-#   else
-#       :scalene
-#   end
-# end
-
-
-# class TriangleError < StandardError
-# end
-
-
-# end
-
-
 require 'pry'
+
 class Triangle
+  # write code here
+
   attr_accessor :side_one, :side_two, :side_three
+
   def initialize(side_one, side_two, side_three)
     @side_one = side_one
     @side_two = side_two
-    @side_three = side_three    
+    @side_three = side_three
   end
-  def invalid?
-    if (side_one + side_two) <= side_three || (side_one + side_three) <= side_two || (side_two + side_three) <= side_one || side_one < 0 || side_two < 0 || side_three < 0
-      true
-    else
-      false
-    end
+
+def valid?
+  if (side_one + side_two) > side_three && (side_two + side_three) > side_one && (side_one + side_three) > side_two && side_one >= 0 && side_two >= 0 && side_three >= 0  
+    true
+  else
+    false
   end
-  def kind
-    if invalid?
-       raise TriangleError
-       puts error.message
-    elsif side_one == side_two && side_two == side_three && side_one == side_three
-        :equilateral
-    elsif side_one == side_two || side_one == side_three || side_two == side_three
-        :isosceles
-    else
-        :scalene
-    end
-  end
-  #binding.pry
-  class TriangleError < StandardError
-    def message
-      "this triangle is invalid"
-    end
+  # binding.pry
+end
+
+def kind
+  if !valid?
+      raise TriangleError
+   elsif side_one == side_two && side_two == side_three && side_one == side_three 
+      :equilateral
+  elsif side_one == side_two || side_one == side_three || side_two == side_three
+      :isosceles
+  else
+      :scalene
   end
 end
+
+
+class TriangleError < StandardError
+end
+
+
+end
+
