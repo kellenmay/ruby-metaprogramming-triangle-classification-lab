80980a24ac7b90d2a5fb70858a861bf49acd7ebf
Kellen <kellenbmay@gmail.com>
2020-12-08 18:19:26 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,37 +1,44 @@
-class Triangle
-  # write code here
+require 'pry'
 
+class Triangle
+  
   attr_accessor :side_one, :side_two, :side_three
 
   def initialize(side_one, side_two, side_three)
     @side_one = side_one
     @side_two = side_two
-    @side_three = side_three
+    @side_three = side_three    
   end
 
-def valid?(side_one, side_two, side_three)
-  if side_one + side_two >= side_three || side_two + side_three >= side_one || side_one + side_three >= side_two && side_one > 0 && side_two > 0 && side_three > 0  
-    true
-  else
-    false
-  end
-end
-
-def kind(side_one, side_two, side_three)
-  if !valid?
-    begin
-      raise TriangleError
-    rescue TriangleError => error
-        puts error.message
+  def invalid?
+    if (side_one + side_two) <= side_three || (side_one + side_three) <= side_two || (side_two + side_three) <= side_one || side_one < 0 || side_two < 0 || side_three < 0
+      true
+    else
+      false
     end
-   elsif side_one == side_two && side_two == side_three && side_one == side_three 
+  end
+
+def kind
+  if invalid?
+     raise TriangleError
+     puts error.message
+  elsif side_one == side_two && side_two == side_three && side_one == side_three
       :equilateral
   elsif side_one == side_two || side_one == side_three || side_two == side_three
       :isosceles
   else
       :scalene
-    end
   end
+end
+
+
+#binding.pry
+class TriangleError < StandardError
+  
+  def message
+    "this triangle is invalid"
+  end
+end
 
 end
 
